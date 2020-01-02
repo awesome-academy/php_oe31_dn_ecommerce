d7ec5d5a6a20e394ec9dfbb6e2c1f9a74f25d09c
duongricky <duongricky113@gmail.com>
2020-01-02 19:52:15 +0700

    edit migrations

---
+++ database/migrations/2020_01_02_062907_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price', 18, 0);
+            $table->decimal('sale_price', 18, 0)->nullable();
+            $table->decimal('sale_percent', 18, 0)->nullable();
+            $table->integer('quantity');
+            $table->unsignedInteger('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
